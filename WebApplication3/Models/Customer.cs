a3329b51b4e6152b1a95779274897bf09daae4c4
Diakonen <oscar_pehrson@msn.com>
2016-01-28 12:57:50 +0100

    Yo

---
+++ WebApplication3/Models/Customer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+
+namespace WebApplication3.Models
+{
+    public class Customer
+    {
+
+        public int CustomerID { get; set; }
+
+        public string FName { get; set; }
+        public string LName { get; set; }
+        public string Adress { get; set; }
+        public int PostNr { get; set; }
+        public string City { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Comment { get; set; }
+
+
+    }
+
+    public class CostumerDBContext : DbContext
+    {
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
---
+++ WebApplication3/Models/Product.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+
+namespace WebApplication3.Models
+{
+    public class Product
+    {
+        
+        public int ProductID { get; set; }
+        
+        public string ArtName { get; set; }
+        public string Descr { get; set; }
+        public int Price { get; set; }
+        public int InStock { get; set; }
+
+    }
+
+    public class ProductDBContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+    }
+}
