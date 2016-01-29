95d62f69627a3ff120d5bcf4ae51ffa2c46067a6
Diakonen <oscar_pehrson@msn.com>
2016-01-29 13:48:19 +0100

    saaf

---
+++ WebApplication3/Models/Order.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication3.Models
+{
+    public class Order
+    {
+        public int OrderID { get; set; }
+
+        public int CustomerID { get; set; }
+        
+    }
+
+
+    public class OrderDBContext : DbContext
+    {
+        public DbSet<Order> Orders { get; set; }
+    }
+}
