ff62bcb7150768f3c5a40ebba788c608a7e394a3
henwist <henwist@gmail.com>
2016-02-02 18:26:49 +0100

    Our shopping chart

---
+++ WebApplication3/Models/ShoppingChart.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication3.Models
+{
+    public class ShoppingChart
+    {
+        private TheDatabase db = new TheDatabase();
+        private List<ChartObject> theChart;
+        private ChartObject chartObject;
+        private Product product;
+
+        public void AddProductToChart(int? id)
+        {
+
+            if (id != null)
+            {
+                product = db.Products.Find(id);
+
+                chartObject.Id = (int)id;
+                chartObject.ProdName =  product.ArtName;
+                chartObject.Price = product.Price;
+
+                theChart.Add(chartObject);
+            }
+            
+        }
+
+        public void  DelProductFromChart(int? id)
+        {
+            if (id != null)
+            {
+
+                //theChart.Find(System.Predicate < int> product);
+            }
+
+        }
+
+        public bool IsProductInChart(int? id)
+        {
+            if (id != null)
+            {
+
+                //theChart.Find(System.Predicate < int> product);
+            }
+
+            return true;
+
+        }
+
+        private class ChartObject
+        {
+            public string ProdName { get; set; }
+
+            public double Price { get; set; }
+
+            public int Id { get; set; }
+        }
+
+   }
+}
