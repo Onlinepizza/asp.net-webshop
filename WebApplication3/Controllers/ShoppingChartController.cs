4048eac2417a64d1cb1d651f7fecbcb28873e239
henwist <henwist@gmail.com>
2016-08-16 10:50:35 -0700

    cookie

--- WebApplication3/Controllers/ShoppingChartController.cs
+++ WebApplication3/Controllers/ShoppingChartController.cs
@@ -8,12 +8,46 @@ using System.Web;
 using System.Web.Mvc;
 using WebApplication3.Models;
 
+
+/*
+ * 
+ *
+ * Convert.ToBase64String(MachineKey.Protect(Encoding.UTF8.GetBytes("your cookie value")))
+
+Encoding.UTF8.GetString(MachineKey.Unprotect(Convert.FromBase64String("your cookie value")))
+
+
+
+    Request.Cookies["someCookie"]
+
+
+
+    HttpCookie cookie = new HttpCookie("search");
+
+will reset the search cookie
+
+To get a cookie:
+
+HttpCookie cookie = HttpContext.Request.Cookies.Get("some_cookie_name");
+
+To check for a cookie's existence:
+
+HttpContext.Request.Cookies["some_cookie_name"] != null
+
+To save a cookie:
+
+HttpCookie cookie = new HttpCookie("some_cookie_name");
+HttpContext.Response.Cookies.Remove("some_cookie_name");
+HttpContext.Response.SetCookie(cookie );
+
+
+ * */
+
 namespace WebApplication3.Controllers
 {
     public class ShoppingChartController : Controller
     {
         private TheDatabase db = new TheDatabase();
-
         // GET: ShoppingChart
         public ActionResult Index()
         {
